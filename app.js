7dc59046237593a8d468603a0eaf615889de08bb
KennethKcc98 <44659208+KennethKcc98@users.noreply.github.com>
2019-08-28 00:36:09 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,16 +4,81 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Kcc98:Kcc981014@cem304-bfaow.mongodb.net/test";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+const conn=MongoClient.connect(uri, function(err, client) {
+    if(err) {
+         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+    }else{
+       var collection = client.db("Staff").collection("Staff");
+        console.log('Connected...');
+        client.close();
+    }
+
+    // perform actions on the collection object
+    
+ });
+//var mongodb = require('mongodb');
+//var dbConn = mongodb.MongoClient.connect(uri);
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname+'/index.html'); 
+})
+app.post('/staff-login', function (req, res){
+    try{
+        MongoClient.connect(uri, function(err, client){
+            if(err) {
+                console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+           }else{
+            var collection = client.db("Staff").collection("Staff");
+            try{
+                collection.findOne({sId:req.body.sId},(function(err,items){
+                    if(items == null){ 
+                        res.send("Login invalid");
+                        console.log("login not sucess");
+                    }else if(items.sId === req.body.sId){
+                        res.sendFile(__dirname+'/manage.html');
+                        console.log("login sucess");
+                    }
+                    //console.log(items);
+                    }))
+            }catch(ex){
+                throw new Error(ex.toString());
+            }
+            } 
+        });
+    }catch(ex){
+        throw new Error(ex.toString());
+    }
 });
- 
-app.post('/submit-student-data', function (req, res) {
-    var sId = req.body.sId ;
- 
-    res.send(sId + ' Submitted Successfully!');
-});
- 
+/*app.post('/submit-student-data', function (req, res) {
+
+try{
+    MongoClient.connect(uri, function(err, client) {
+        if(err) {
+             console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+        }else{
+           var collection = client.db("Staff").collection("Staff");
+           collection.insert(req.body);
+           console.log('add');
+            client.close();
+        }
+    
+        // perform actions on the collection object
+        
+     });
+}catch(ex){
+    throw new Error(ex.toString());
+}
+            
+   
+     
+   // res.send('Data received:\n' + JSON.stringify(req.body));
+});*/
+
 var server = app.listen(3001, function () {
     console.log('Node server is running..');
 });
--- moserver.js
+++ moserver.js
@@ -1,13 +1,17 @@
 var MongoClient = require('mongodb').MongoClient;
+//var sId = req.body.sId ;
  
-// Connect to the db
+/// Connect to the db
 MongoClient.connect("mongodb://localhost:27017/mymondb", function (err, db) {
   if(err) throw err;
   //Write databse Insert/Update/Query code here..
-  db.collection('Persons',function(err,collection){
-    collection.insert({ id:1, firstName:'Steve', lastName:'Jobs' });
-    collection.insert({ id:2, firstName:'Bill', lastName:'Gates' });
-    collection.insert({ id:3, firstName:'James', lastName:'Bond' });
+  db.collection('Staff',function(err,collection){
+    collection.insert({ id:1001, firstName:'Steve', lastName:'Jobs' });
+    collection.insert({ id:1002, firstName:'Bill', lastName:'Gates' });
+    collection.insert({ id:1003, firstName:'James', lastName:'Bond' });
+    collection.insert({ id:1004, firstName:'Kenneth', lastName:'Chan' });
+    collection.insert({ id:1005, firstName:'Ben', lastName:'Lai' });
+
  
     collection.count(function(err,count){
         if(err) throw err;
